221d62d8899520f32f0061db6582dd5adf07d7df
Tristan Batchler <info@tbat.me>
2026-08-12 07:58:18 +1000

    encode and decode treatment for structures

--- demo_projects/moonlapse/shared/packets/__init__.py
+++ demo_projects/moonlapse/shared/packets/__init__.py
@@ -5,7 +5,7 @@ from demo_projects.moonlapse.shared.packets.proto import PacketProto
 
 def deserialize(data: bytes) -> PacketProto:
     header: bytes = data[:2]
-    packet_type: int = int.from_bytes(header, "big")
+    packet_type: int = int.from_bytes(header)
     payload_bytes = data[2:]
     for registry in (serverbound.REGISTRY, clientbound.REGISTRY):
         if packet_type in registry:
--- demo_projects/moonlapse/shared/packets/proto.py
+++ demo_projects/moonlapse/shared/packets/proto.py
@@ -5,10 +5,18 @@ import struct
 from typing import ClassVar, Protocol, runtime_checkable
 
 
-def _clean_structure(structure: Iterable[object]) -> Iterable[object]:
+def _encode_structure(structure: Iterable[object]) -> Iterable[object]:
     for obj in structure:
         if isinstance(obj, str):
             yield obj.encode()
+        else:
+            yield obj
+
+
+def _decode_structure(structure: Iterable[object]) -> Iterable[object]:
+    for obj in structure:
+        if isinstance(obj, bytes):
+            yield obj.rstrip(b"\x00").decode()
         else:
             yield obj
 
@@ -24,12 +32,12 @@ class PacketProto(Protocol):
     @classmethod
     def from_bytes(cls, data: bytes) -> PacketProto:
         fmt = cls.FORMAT_STRING
-        structure = _clean_structure(struct.unpack(fmt, data))
+        structure = _decode_structure(struct.unpack(fmt, data))
         return cls(*structure)
 
     def serialize(self) -> bytes:
         ptype = self.TYPE
         header_bytes = struct.pack(">H", ptype)
-        structure = _clean_structure(self.get_structure())
+        structure = _encode_structure(self.get_structure())
         payload_bytes = struct.pack(self.FORMAT_STRING, *structure)
         return header_bytes + payload_bytes
